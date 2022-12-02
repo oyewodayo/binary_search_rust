7c2bf1f6b582d894ed29fdab16fa03cc7f33a2a9
Oyewo Temidayo <oyewodayo@gmail.com>
2022-12-02 16:54:09 +0100

    Binary search algorithm with rustlang
    Implementing binary search algorithm using rust language

---
+++ target/debug/binary_search_rust.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Desktop\Rust\binary_search_rust\target\debug\binary_search_rust.exe: C:\Users\HP\Desktop\Rust\binary_search_rust\src\main.rs
---
+++ target/debug/deps/binary_search_rust.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\Rust\binary_search_rust\target\debug\deps\binary_search_rust.exe: src\main.rs
+
+C:\Users\HP\Desktop\Rust\binary_search_rust\target\debug\deps\binary_search_rust.d: src\main.rs
+
+src\main.rs:
